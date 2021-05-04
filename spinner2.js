2bf18f1e8b69cc5323b45b8476439a21bb46beca
Claire Devlin <71520213+cldevlin@users.noreply.github.com>
2021-05-04 14:04:48 -0400

    removed extra comments

--- spinner2.js
+++ spinner2.js
@@ -5,20 +5,3 @@ for (let i = 0; i < 6; i++) {
     process.stdout.write(symbols[i]);
   }, i * 200 - 100)
 }
-
-// setTimeout(() => {
-//   process.stdout.write('\r/   ');
-// }, 300)
-// setTimeout(() => {
-//   process.stdout.write('\r-   ');
-// }, 500)
-// setTimeout(() => {
-//   process.stdout.write('\r\\   ');
-// }, 700)
-// setTimeout(() => {
-//   process.stdout.write('\r|   ');
-// }, 900)
-// setTimeout(() => {
-//   process.stdout.write('\n');
-// }, 1100)
-
